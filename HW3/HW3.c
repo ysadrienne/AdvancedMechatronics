e0b2e7868cd1f3960c4373a7db984de5377db5ab
ysadrienne <adrienneyoon@gmail.com>
2026-04-09 11:31:21 -0500

    Added functions

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -9,23 +9,38 @@
 #define I2C_SDA 8
 #define I2C_SCL 9
 
-
+void initI2C();
+void setPin(unsigned char, unsigned char, unsigned char);
+unsigned char readPin(unsigned char, unsigned char);
 
 int main()
 {
     stdio_init_all();
+    initI2C();
 
-    // I2C Initialisation. Using it at 400Khz.
+
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
+
+
+void initI2C() {
     i2c_init(I2C_PORT, 400*1000);
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+}
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+void setPin(unsigned char address, unsigned char register, unsigned char value) {
+    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+}
+
+unsigned char readPin(unsigned char address, unsigned char register) {
+    i2c_write_blocking(i2c_default, ADDR, &reg, 1, true); 
+    i2c_read_blocking(i2c_default, ADDR, &buf, 1, false);
 }
