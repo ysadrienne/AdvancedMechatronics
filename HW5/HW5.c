103342606fa411607eb0188e443e596569e044a3
ysadrienne <adrienneyoon@gmail.com>
2026-04-16 22:57:05 -0500

    add mpu6050.c

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
-
-
+#include "ssd1306.h"
+#include "mpu6050.h"
 
 int main()
 {
     stdio_init_all();
+    picoLEDInit();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    while (true) {
-        printf("Hello, world!\n");
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+
+    ssd1306_setup();
+    ssd1306_clear();
+    ssd1306_update();
+
+    struct repeating_timer timer;
+    add_repeating_timer_ms(1000, heartbeat_callback, NULL, &timer);
+
+    // unsigned char who = readPin(MPU6050_ADDR, WHO_AM_I);
+    initMPU();
+
+    float ax, ay, az, gx, gy, gz, temp;
+
+    while (true) {   
+        /*
+        printf("Who: 0x%X\r\n", who);
+        sleep_ms(1000);
+        */
+        readMPU(&ax, &ay, &az, &gx, &gy, &gz, &temp);
+        printf("ax: %.2f, ay: %.2f, az: %.2f\r\n", ax, ay, az);
+        printf("gx: %.2f, gy: %.2f, gz: %.2f, temp: %.2f\r\n", gx, gy, gz, temp);
         sleep_ms(1000);
     }
 }
