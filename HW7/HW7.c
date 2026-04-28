846e6d8ebd3a30ec9b615253f53a9849d0d81adb
ysadrienne <adrienneyoon@gmail.com>
2026-04-27 23:25:25 -0500

    complete hw7

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
-#include "math.h"
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -12,14 +12,28 @@
 #define PICO_DEFAULT_SPI_TX_PIN 19
 #define PIN_CS   17
 
+#define pi 3.141592653589793238
+
+void setDac(int channel, float v);
+
+int sine_i = 0;
+int tri_i = 0;
+int counter = 0;
+
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(PIN_CS, 0);
+    asm volatile("nop \n nop \n nop");
+}
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(PIN_CS, 1);
+    asm volatile("nop \n nop \n nop");
+}
 
 int main()
 {
     stdio_init_all();
-
-    // init spi pins and peripheral
-    // init cs pin
-    // set cs pin high
 
     // SPI initialisation. This example will use SPI at 1MHz.
     spi_init(SPI_PORT, 1000*1000);
@@ -27,40 +41,55 @@ int main()
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN,  GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
     
-    cs_select(PIN_CS);
-    spi_write_blocking(SPI_PORT, data, 2); // where data is a uint8_t array with length len
-    cs_deselect(PIN_CS);
-   
+    gpio_init(PIN_CS);
+    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_put(PIN_CS, 1);
+    
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    float v[100];
+    float v_sine[100];
     for (int i = 0; i < 100; i++) {
-        v[i] = (sine(2 * pi * f * t) + 1)/2 * 3.3;
+        v_sine[i] = (sin(2 * pi * i / 100.0f) + 1.0f)/2.0f * 3.3f;
+    }
+
+    float v_tri[100];
+    for (int i = 0; i < 100; i++) {
+        if (i < 50) {
+             v_tri[i] += (i / 50.0f) * 3.3f;
+        } else {
+            v_tri[i] = ((100 - i) / 50.0f) * 3.3f;
+        }
     }
 
     while (true) {
         // update DAC
-        float t = 0;
-        t = t+0.01
-        // float voltageA = (sine(2 * pi * f * t) + 1)/2 * 3.3; // f = 2 Hz
-        float v = 0;
-        setDac(0, voltageA);
-        //setDAC(1, voltageB);
-        sleep_ms(10);
+        setDac(0, v_sine[sine_i]);
+        sine_i++;
+        if (sine_i >= 100) {
+            sine_i = 0;
+        }
+
+        if (counter % 2 == 0) {
+            setDac(1, v_tri[tri_i]);
+            tri_i++;
+            if (tri_i >= 100) {
+                tri_i = 0;
+            }
+        }
+        counter++;
+        sleep_ms(5);
     }
 }
 
 void setDac(int channel, float v) {
-
-    uint8_t data[2];
-    data[0] = 0b01110000;
-    data[0] = data[0]| ((channel & 0b1) << 7) // put channel in spot
     uint16_t theV = v/3.3 * 1023; // 0b11111111
+    uint8_t data[2];
+    
+    data[0] = (channel == 0) ? 0x30 : 0xB0;
     data[0] = data[0] | ((theV >> 6) & 0b00001111);
     data[1] = (theV << 2) & 0xFF; // 0b11111100
 
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, 2); // where data is a uint8_t array with length len
     cs_deselect(PIN_CS);
-
 }
